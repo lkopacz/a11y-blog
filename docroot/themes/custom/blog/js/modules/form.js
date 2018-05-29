70fa0cbf7d8cd76f0098836ec423636281fb8ffb
Lindsey Kopacz <lkopacz@users.noreply.github.com>
2018-05-29 19:47:46 -0400

    BLOG-16: Added footer styling, social media styling.

---
+++ docroot/themes/custom/blog/js/modules/form.js
@@ -0,0 +1,37 @@
+/**
+ * @file
+ * Navigation javascript file for accessibility and toggling.
+ */
+
+((Drupal) => {
+    'use strict';
+    Drupal.behaviors.form = {
+      attach: (context, settings) => {
+        if (typeof context['location'] !== 'undefined') {
+        const inputWrappers = document.querySelectorAll('.form-item');
+
+        for (let item of inputWrappers) {
+            const input = item.querySelector('input');
+            if (input === null) {
+                item.querySelector('label').classList.add('focus');
+            }
+            else if (input.getAttribute('type') === 'text') {
+                input.addEventListener('focus', function () {
+                    input.previousElementSibling.classList.add('focus');
+                }, false);
+                input.addEventListener('blur', function () {
+                    const value = input.value;
+                    if (value.length === 0) {
+                        input.previousElementSibling.classList.remove('focus');
+                    }
+                    else {
+                        input.previousElementSibling.classList.add('focus');
+                    }
+                }, false);
+            }
+        }
+
+        }
+      }
+    }
+})(Drupal);
