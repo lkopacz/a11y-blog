1c4019be4e07c30b4f21e044eab167574e0ea121
Lindsey Kopacz <lkopacz@users.noreply.github.com>
2018-06-18 08:16:01 -0400

    BLOG-23: Fixing js console error. Adding styles to make social media links more responsive.

--- docroot/themes/custom/blog/js/modules/navigation.js
+++ docroot/themes/custom/blog/js/modules/navigation.js
@@ -8,51 +8,52 @@
 
   Drupal.behaviors.navigation = {
     attach: (context, settings) => {
+      if (typeof context['location'] !== 'undefined') {
 
-      function NAVIGATION(mainNavClass, hamburgerToggleClass, headerClass) {
-        this.mainNavClass = mainNavClass;
-        this.hamburgerToggleClass = hamburgerToggleClass;
-        this.headerClass = headerClass;
+        function NAVIGATION(mainNavClass, hamburgerToggleClass, headerClass) {
+          this.mainNavClass = mainNavClass;
+          this.hamburgerToggleClass = hamburgerToggleClass;
+          this.headerClass = headerClass;
 
-        this.mainNav = context.querySelector(`.${this.mainNavClass}`);
-        this.hamburgerToggle = context.querySelector(`.${this.hamburgerToggleClass}`);
-        this.header = context.querySelector(`.${this.headerClass}`);
-        this.mainNavLinks = this.mainNav.getElementsByTagName('a');
-        this.lastNavLink = this.mainNavLinks[this.mainNavLinks.length - 1];
+          this.mainNav = context.querySelector(`.${this.mainNavClass}`);
+          this.hamburgerToggle = context.querySelector(`.${this.hamburgerToggleClass}`);
+          this.header = context.querySelector(`.${this.headerClass}`);
+          this.mainNavLinks = this.mainNav.getElementsByTagName('a');
+          this.lastNavLink = this.mainNavLinks[this.mainNavLinks.length - 1];
 
-        this.toggleMenu = function () {
+          this.toggleMenu = function () {
 
-          this.header.classList.toggle('open');
-          this.hamburgerToggle.classList.toggle('open');
-
-          for (let link of this.mainNavLinks) {
-            link.tabIndex = 0;
-          }
-        }
-
-        this.exitMenu = function (e) {
-          if (e.type === 'blur' || e.keyCode === 27) {
-
-            this.header.classList.remove('open');
-            this.hamburgerToggle.classList.remove('open');
+            this.header.classList.toggle('open');
+            this.hamburgerToggle.classList.toggle('open');
 
             for (let link of this.mainNavLinks) {
-              link.tabIndex = -1;
+              link.tabIndex = 0;
             }
           }
+
+          this.exitMenu = function (e) {
+            if (e.type === 'blur' || e.keyCode === 27) {
+
+              this.header.classList.remove('open');
+              this.hamburgerToggle.classList.remove('open');
+
+              for (let link of this.mainNavLinks) {
+                link.tabIndex = -1;
+              }
+            }
+          }
+
+          for (let link of this.mainNavLinks) {
+            link.tabIndex = -1;
+            link.addEventListener('keyup', this.exitMenu.bind(this), false);
+          }
+
+          this.hamburgerToggle.addEventListener('click', this.toggleMenu.bind(this), false);
+          this.lastNavLink.addEventListener('blur', this.exitMenu.bind(this), false);
         }
 
-        for (let link of this.mainNavLinks) {
-          link.tabIndex = -1;
-          link.addEventListener('keyup', this.exitMenu.bind(this), false);
-        }
-
-        this.hamburgerToggle.addEventListener('click', this.toggleMenu.bind(this), false);
-        this.lastNavLink.addEventListener('blur', this.exitMenu.bind(this), false);
+        const menu = new NAVIGATION('menu', 'hamburger-menu', 'header');
       }
-
-      const menu = new NAVIGATION('menu', 'hamburger-menu', 'header');
-
     }
   }
 })(Drupal);
